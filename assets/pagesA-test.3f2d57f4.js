6a48d8ad6b62303da55344659be4a30e2658351f
dhjz <909514844@qq.com>
2025-08-30 12:44:12 +0800

    pages

---
+++ assets/pagesA-test.3f2d57f4.js
@@ -0,0 +1,1 @@
+import{_ as n,m as s,n as t,p as a}from"./index-c84eb950.js";const e=n({},[["render",function(n,e){const o=a;return s(),t(o,{class:"page-content"})}]]);export{e as default};
--- src/config.js
+++
@@ -1,6 +0,0 @@
-export const SYS_CONFIG = Object.assign({
-  baseApi: 'http://home.xyz:25244', // http://test.alist:25244
-  musicDir: '/Local/Music',
-  username: '',
-  password: ''
-}, uni.getStorageSync('SYS_CONFIG') || {})
